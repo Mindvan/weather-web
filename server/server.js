f274becf235e55e34500cacc617b297d203618a2
Ivan Eroshin <itseroshin@gmail.com>
2022-07-04 00:41:06 +0300

    fix client folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require("cors")
-const port = 5000
+const port = 8000
 
 const fetch = require("isomorphic-fetch");
 const cheerio = require("cheerio");
